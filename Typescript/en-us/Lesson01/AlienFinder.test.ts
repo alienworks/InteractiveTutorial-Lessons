06c5a3e5bac21e69ff22a12142292a44c4ccefd1
Hu Shili <799498265@qq.com>
2020-04-27 20:27:59 +0800

    TypeScript L01, 02 updated. (#20)
    * Move some tests to L01Ch09
    
    * L02Ch01,03 updated
    
    * L02Ch04
    
    * L02Ch05
    
    * L02 script updated
    
    * L01 script updated
    
    * Related content updated
    
    * Format fix
    
    * Final chapter in L02
    
    * L02Ch04

--- Typescript/en-us/Lesson01/AlienFinder.test.ts
+++ Typescript/en-us/Lesson01/AlienFinder.test.ts
@@ -1,14 +1,23 @@
 import { withContracts } from '../neo-one/test';
 
 describe('AlienFinder', () => {
-  test('exist', async () => {
+  test('definition', async () => {
     await withContracts(async ({ alienFinder }) => {
       expect(alienFinder).toBeDefined();
     });
   });
-  test('invoke', async () => {
+  test('function', async () => {
     await withContracts(async ({ alienFinder }) => {
       const receipt = await alienFinder.generateAlien.confirmed('someone');
+      if (receipt.result.state === 'FAULT') {
+        throw new Error(receipt.result.message);
+      }
+      expect(receipt.result.state).toEqual('HALT');
+      expect(receipt.result.value).toBeUndefined();
+      expect(receipt.events).toHaveLength(1);
+      let event = receipt.events[0];
+      expect(event.name).toEqual('generate');
+      expect(event.parameters.id.toNumber()).toEqual(1);
     });
   });
 });
