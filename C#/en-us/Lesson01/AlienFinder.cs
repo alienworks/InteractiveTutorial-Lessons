c77cfa26003fda68020c7d1928a91617c622da97
Fortunesky <Fortunesky>
2020-06-24 15:25:22 +0800

    1. refactor csharp lessons' chapter number; 2. format csharp smart contract;

--- C#/en-us/Lesson01/AlienFinder.cs
+++ C#/en-us/Lesson01/AlienFinder.cs
@@ -1,37 +1,31 @@
-using Neo.SmartContract.Framework;
-using Neo.SmartContract.Framework.Services.Neo;
 using System;
 using System.Numerics;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
 
-public class AlienFinder : SmartContract
-{
-    public class Alien
-    {
+public class AlienFinder : SmartContract {
+    public class Alien {
         public uint xna;
         public string alienName;
     }
 
-    public static void Main(string alienName) 
-    {
-        ulong randomNumber = RandomNumber(); 
-        uint xna = FindXna(randomNumber);
-        Alien someAlien = new Alien
-        {
-            xna = xna, 
-            alienName = alienName, 
+    public static void Main (string alienName) {
+        ulong randomNumber = RandomNumber ();
+        uint xna = FindXna (randomNumber);
+        Alien someAlien = new Alien {
+            xna = xna,
+            alienName = alienName,
         };
-        Runtime.Notify(alienName, "created!");
+        Runtime.Notify (alienName, "created!");
     }
 
-    private static ulong RandomNumber()
-    {
-        uint blockHeight = Blockchain.GetHeight();
-        return Blockchain.GetHeader(blockHeight).ConsensusData; 
+    private static ulong RandomNumber () {
+        uint blockHeight = Blockchain.GetHeight ();
+        return Blockchain.GetHeader (blockHeight).ConsensusData;
     }
 
-    private static uint FindXna(ulong randomNumber)
-    {
-        return (uint)(randomNumber % 100000000);
+    private static uint FindXna (ulong randomNumber) {
+        return (uint) (randomNumber % 100000000);
     }
-    
+
 }
--- C#/zh-cn/Lesson01/AlienFinder.cs
+++ C#/zh-cn/Lesson01/AlienFinder.cs
@@ -1,44 +1,38 @@
-using Neo.Smartcontract.Framework;
-using Neo.SmartContract.Framework.Services.Neo;
 using System;
 using System.Numerics;
+using Neo.Smartcontract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
 
-public class AlienFinder : Smartcontract
-{
-    public class Alien
-    {
-        public static uint counter = 0; 
+public class AlienFinder : Smartcontract {
+    public class Alien {
+        public static uint counter = 0;
         public uint xna;
         public string alienName;
         public uint blockHeight;
-        public uint id; 
+        public uint id;
 
-        public Alien(uint xna, string alienName, uint blockHeight) 
-        {
-            this.xna = xna; 
+        public Alien (uint xna, string alienName, uint blockHeight) {
+            this.xna = xna;
             this.alienName = alienName;
             this.blockHeight = blockHeight;
             counter = counter + 1;
-            this.id = counter; 
+            this.id = counter;
         }
     }
 
-    public static void Main(string alienName) 
-    {
-        uint blockHeight = Blockchain.GetHeight();
-        uint xna = FindXna(RandomNumber(blockHeight));
-        Alien someAlien = new Alien(xna, alienName, blockHeight);
-        Runtime.Notify("Alien created, ID: " + someAlien.id);
+    public static void Main (string alienName) {
+        uint blockHeight = Blockchain.GetHeight ();
+        uint xna = FindXna (RandomNumber (blockHeight));
+        Alien someAlien = new Alien (xna, alienName, blockHeight);
+        Runtime.Notify ("Alien created, ID: " + someAlien.id);
     }
 
-    private static ulong RandomNumber(uint blockHeight)
-    {
-        return Blockchain.GetHeader(blockHeight).ConsensusData; 
+    private static ulong RandomNumber (uint blockHeight) {
+        return Blockchain.GetHeader (blockHeight).ConsensusData;
     }
 
-    private static uint FindXna(ulong randomNumber)
-    {
-        return (uint)(randomNumber % 100000000);
+    private static uint FindXna (ulong randomNumber) {
+        return (uint) (randomNumber % 100000000);
     }
-    
+
 }
