2dd1db5191eeb37e3e98963a5919b1af3f319dbd
Valilian <>
2020-07-01 12:00:17 +0800

    update C# zh-cn L01-L04

--- C#/zh-cn/Lesson02/AlienFinder-L2.cs
+++ C#/zh-cn/Lesson02/AlienFinder-L2.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Numerics;
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
@@ -5,61 +6,50 @@ using Neo.SmartContract.Framework.Services.Neo;
 using Helper = Neo.SmartContract.Framework.Helper;
 
 public class AlienFinder : SmartContract {
-    // type name for the contract
-    const string AlienMapName = nameof (AlienFinder);
+
+    public delegate void deleAlienEvent (BigInteger id);
+    public static event deleAlienEvent AlienGenerated;
+    public static event deleAlienEvent AlienDeleted;
 
     public class Alien {
-        private static BigInteger counter = 0;
-
         public uint Xna { get; set; }
         public string AlienName { get; set; }
-        public uint BlockHeight { get; private set; }
-        public BigInteger Id { get; private set; }
-
-        public Alien (uint xna, string alienName, uint blockHeight) {
-            Xna = xna;
-            AlienName = alienName;
-            BlockHeight = blockHeight;
-
-            byte[] value = Storage.Get (Storage.CurrentContext, "counter");
-            if (value != null) { counter = value.ToBigInteger (); } else { counter = 0; }
-
-            Id = ++counter;
-            Storage.Put (Storage.CurrentContext, "counter", counter);
-        }
+        public BigInteger Id { get; set; }
     }
 
-    public static object Main (string operation, params object[] args) {
-        if (Runtime.Trigger == TriggerType.Verification) {
-            return false;
+    public static object Main (string method, params object[] args) {
+        if (Runtime.Trigger == TriggerType.Application) {
+            switch (method) {
+                case "generateAlien":
+                    return GenerateAlien ((string) args[0], (byte[]) args[1]);
+                case "query":
+                    return Query ((byte[]) args[0]);
+            }
         }
-        switch (operation) {
-            case "generateAlien":
-                return GenerateAlien ((string) args[0], (byte[]) args[1]);
-            case "query":
-                return Query ((byte[]) args[0]);
-            case "mutate":
-                return Mutate ((byte[]) args[0], (uint) args[1]);
-            default:
-                return false;
-        }
+        return false;
     }
 
-    public static bool GenerateAlien (string alienName, byte[] owner) {
-        // Check if the `owner` argument is the same as one who invoked contract
-        if (!Runtime.CheckWitness (owner)) return false;
+    public static BigInteger GenerateAlien (string alienName, byte[] owner) {
+        if (owner.Length != 20 && owner.Length != 33)
+            throw new InvalidOperationException ("The parameter owner should be a 20-byte address or a 33-byte public key"); // Check if the owner is the same as one who invoked contract
+        if (!Runtime.CheckWitness (owner)) return 0;
 
-        uint blockHeight = Blockchain.GetHeight ();
-        uint xna = FindXna (RandomNumber (blockHeight));
-        Alien someAlien = new Alien (xna, alienName, blockHeight);
+        uint xna = FindXna (RandomNumber ());
+        Alien someAlien = new Alien {
+            Xna = xna,
+            AlienName = alienName,
+            Id = updateCounter ()
+        };
+
         // add the object to storage
-        StorageMap alienMap = Storage.CurrentContext.CreateMap (AlienMapName);
+        StorageMap alienMap = Storage.CurrentContext.CreateMap (nameof (alienMap));
         alienMap.Put (someAlien.Id.ToByteArray (), Helper.Serialize (someAlien));
-        Runtime.Notify ("Alien created, ID: " + (someAlien.Id));
-        return true;
+        Runtime.Notify (someAlien.Id, "created");
+        return someAlien.Id;
     }
 
-    private static ulong RandomNumber (uint blockHeight) {
+    private static ulong RandomNumber () {
+        uint blockHeight = Blockchain.GetHeight ();
         return Blockchain.GetHeader (blockHeight).ConsensusData;
     }
 
@@ -67,49 +57,39 @@ public class AlienFinder : SmartContract {
         return (uint) (randomNumber % 100000000);
     }
 
+    // Read the counter from storage if it exists. Otherwise, default to 0. 
+    private static BigInteger getCounter () {
+        BigInteger counter = 0;
+        byte[] value = Storage.Get ("alienCount");
+        if (value.Length != 0)
+            counter = value.ToBigInteger ();
+        return counter;
+    }
+
+    private static BigInteger updateCounter () {
+        BigInteger counter = getCounter ();
+        counter = counter + 1;
+        Storage.Put (Storage.CurrentContext, "alienCount", counter);
+        return counter;
+    }
+
     public static Alien Query (byte[] id) {
         // deserialise the alien object then return it
-        StorageMap alienMap = Storage.CurrentContext.CreateMap (AlienMapName);
+        StorageMap alienMap = Storage.CurrentContext.CreateMap (nameof (alienMap));
         var result = alienMap.Get (id);
         if (result.Length == 0) return null;
         return Helper.Deserialize (result) as Alien;
     }
 
-    /*
-     * Increase the desired digits of an alien xna by a random number
-     * the other fields will decrease so that the sum of attributes remain constant. 
-     * The first two digits (which represent life form) does not count as attribute. 
-     */
-    public static void Mutate (byte[] id, uint attribute) {
-        Alien a = Query (id);
-        if (a == null) {
-            Runtime.Notify ("Alien does not exist! ");
-            return;
-        }
-        uint blockHeight = Blockchain.GetHeight ();
-        uint randomDigit = (uint) (RandomNumber (blockHeight) % 10);
-        switch (attribute) {
-            case 3: // weight
-                /* 
-                 * Add twice the rng to the desired field, then substract
-                 * equal amount from other fields. 
-                 */
-                a.Xna += randomDigit * 2;
-                a.Xna -= randomDigit * 100;
-                a.Xna -= randomDigit * 10000;
-                return;
-            case 2: // speed
-                a.Xna += randomDigit * 2 * 100;
-                a.Xna -= randomDigit;
-                a.Xna -= randomDigit * 10000;
-                return;
-            case 1: // strength 
-                a.Xna += randomDigit * 2 * 10000;
-                a.Xna -= randomDigit;
-                a.Xna -= randomDigit * 100;
-                return;
-            default:
-                return;
-        }
+    public static bool Delete (byte[] owner, byte[] id) {
+        if (owner.Length != 20 && owner.Length != 33)
+            throw new InvalidOperationException ("The parameter owner should be a 20-byte address or a 33-byte public key"); // Check if the owner is the same as one who invoked contract
+        // Check if the owner is the same as one who invoked contract
+        if (!Runtime.CheckWitness (owner)) return false;
+
+        StorageMap alienMap = Storage.CurrentContext.CreateMap (nameof (alienMap));
+        alienMap.Delete (id);
+        AlienDeleted (id.ToBigInteger ());
+        return true;
     }
 }
